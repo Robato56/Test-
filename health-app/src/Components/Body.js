5b3bec01bdb21df275781766207b112eed2998ea
Joseph Kakai Ngalu <josephngalu96@gmail.com>
2024-07-21 23:45:21 +0000

    Created body component with cards, graph and user table

---
+++ health-app/src/Components/Body.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Graph from "./Graph";
+import Cards from "./Cards";
+import UserTable from "./UserTable";
+import Card from 'react-bootstrap/Card';
+
+
+function Body() {
+    return (
+        <div style={{backgroundColor: "#EDEADE"}}>
+            <br />
+            <Card className= "rounded bg-white">
+                <Card.Body>
+                    <h2 className="text-left">Diagnosis History</h2>
+                    <br />
+                    <br />
+                    <Graph />
+                    <Cards />
+                </Card.Body>
+            </Card>
+            <br />
+            <Card className= "rounded bg-white">
+                <Card.Body>
+                <h2 className="text-left">Diagnostic List</h2>
+                <br />
+                <br />
+                <br />
+                    <UserTable />
+                </Card.Body>
+            </Card>
+            <br />
+        </div>
+    );
+}   
+
+export default Body;
